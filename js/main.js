fe51cc3974c5e2370d5f9e46ddf4126de455c080
uidev-appac <uidev.appac@gmail.com>
2022-06-23 15:48:31 +0530

    changed to pages

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("../sw_cached_site.js")
+      .register("../sw_cached_pages.js")
       .then((reg) => {
         console.log("Service Worker: Registered (scope: " + reg.scope + ")");
       })
